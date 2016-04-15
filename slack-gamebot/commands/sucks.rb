f6a526d8f36c199ccae7e331a214d40f55670010
Alex Laviolette <alex.laviolette92@gmail.com>
2016-04-15 15:10:57 -0400

    mu

--- slack-gamebot/commands/sucks.rb
+++ slack-gamebot/commands/sucks.rb
@@ -3,7 +3,9 @@ module SlackGamebot
     class Sucks < SlackRubyBot::Commands::Base
       command 'sucks', 'suck', 'you suck', 'sucks!', 'you suck!' do |client, data, _match|
         user = ::User.find_create_or_update_by_slack_id!(client, data.user)
-        if user.losses && user.losses > 5
+        if user.user_name == mu
+          client.say(channel: data.channel, text: "yes... you are right :(")
+        elsif user.losses && user.losses > 5
           client.say(channel: data.channel, text: "No <@#{data.user}>, with #{user.losses} losses, you suck!", gif: 'loser')
         elsif user.rank && user.rank > 3
           client.say(channel: data.channel, text: "No <@#{data.user}>, with a rank of #{user.rank}, you suck!", gif: 'loser')
