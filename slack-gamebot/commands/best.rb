057e8075b40a536eb15888ed7c556990dee34a16
Alex Laviolette <alex.laviolette92@gmail.com>
2016-04-15 14:20:56 -0400

    best

---
+++ slack-gamebot/commands/best.rb
@@ -0,0 +1,10 @@
+module SlackGamebot
+  module Commands
+    class Best < SlackRubyBot::Commands::Base
+      command 'best', 'who is the best', 'greatest', 'who is the greatest', 'winner' do |client, data, _match|
+        client.say(channel: data.channel, text: "Alex is the best. In fact, he might be the greatest player to ever rule rocket league. Bow down before his aerial might.")
+        logger.info "SUCKS: #{client.owner} - #{data.user}"
+      end
+    end
+  end
+end
