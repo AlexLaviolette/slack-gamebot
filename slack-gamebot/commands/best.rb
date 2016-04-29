9424a14872f989d18f230b7e63f12915017496ef
Alex Laviolette <alex.laviolette92@gmail.com>
2016-04-29 16:57:25 -0400

    best 2.0

--- slack-gamebot/commands/best.rb
+++ slack-gamebot/commands/best.rb
@@ -2,8 +2,22 @@ module SlackGamebot
   module Commands
     class Best < SlackRubyBot::Commands::Base
       command 'best', 'who is the best', 'who is the best?', 'greatest', 'who is the greatest', 'who is the greatest?', 'winner' do |client, data, _match|
-        client.say(channel: data.channel, text: "Alex is the best. In fact, he might be the greatest player to ever rule rocket league. Bow down before his aerial might.")
-        logger.info "SUCKS: #{client.owner} - #{data.user}"
+        user = ::User.find_create_or_update_by_slack_id!(client, data.user)
+        
+        ranked_players = client.owner.users.ranked
+        if ranked_players.any?
+          best = ranked_players.send(:asc, :rank).first
+          if user == best
+            client.say(channel: data.channel, text: "You! You are the best #{user.user_name}! Congratulations on your unreasonably high skill-level and ridiculously good looks!")
+          elsif user.rank && user.rank <= 3
+            client.say(channel: data.channel, text: "#{best.user_name} is the best, but with a rank of #{user.rank}, you aren't too bad. But then again... if you aren't first, you're last!", gif: 'loser')
+          else
+            client.say(channel: data.channel, text: "#{best.user_name} is the best, and you are \##{user.rank}... so you probably have no real chance of ever holding the coveted stop spot....   loser.", gif: 'rude')
+          end
+        else
+          client.say(channel: data.channel, text: "There're no ranked players.", gif: 'empty')
+        end
+        logger.info "WORST: #{client.owner} - #{data.user}"
       end
     end
   end
