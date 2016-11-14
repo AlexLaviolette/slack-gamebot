8e496d8153c48151407da6992effb69527d7c0dc
Alex Laviolette <alex.laviolette92@gmail.com>
2016-11-14 16:37:47 -0500

    Remove rank

--- slack-gamebot/commands/setrank.rb
+++ slack-gamebot/commands/setrank.rb
@@ -1,6 +1,6 @@
 module SlackGamebot
   module Commands
-    class SetNick < SlackRubyBot::Commands::Base
+    class SetRank < SlackRubyBot::Commands::Base
       def self.call(client, data, match)
         auth = ::User.find_create_or_update_by_slack_id!(client, data.user)
         expression = match['expression'] if match['expression']
@@ -16,9 +16,9 @@ module SlackGamebot
           client.say(channel: data.channel, text: "You're not a captain, sorry.", gif: 'sorry')
           logger.info "SetRank: #{client.owner} - #{auth.user_name} setrank #{user.user_name}, failed, not captain"
         else
-          user.set_rl_rank!(rank)
+          # user.set_rl_rank!(rank)
           client.say(channel: data.channel, text: "#{user.user_name} rank set to #{rank}", gif: 'power')
-          logger.info "SetRank: #{client.owner} - #{auth.user_name} set rank for #{user.user_name} to #{user.rl_rank}"
+          # logger.info "SetRank: #{client.owner} - #{auth.user_name} set rank for #{user.user_name} to #{user.rl_rank}"
         end
       end
     end
